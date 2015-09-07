b5a6c61bd74484446b2b4a72213a29f12815b9ae
Yusuke Takahashi <youthkee@gmail.com>
2015-09-08 08:32:42 +0900

    Adapt JS to resizing.

--- teaser/dist/js/script.js
+++ teaser/dist/js/script.js
@@ -1,7 +1,6 @@
 $(function(){
 
 var wH = $(window).height();
-var wW = $(window).width(); 
 var mH = 500;
 var fH = $('#footer').height();
 
@@ -10,5 +9,14 @@ if (wH < 500) {
 	} else {
 	$('#content').css('height',wH - fH);
 }
+
+$(window).resize(function(){
+	var wH = $(window).height();
+	if (wH < 500) {
+		$('#content').css('height',500 - fH);
+		} else {
+		$('#content').css('height',wH - fH);
+	}
+});
 
 });
