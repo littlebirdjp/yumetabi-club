9c8fda7eaedaf6b535cad24f9f7527969c2bf759
Yusuke Takahashi <youthkee@gmail.com>
2017-08-09 15:27:06 +0900

    Build 10th Anniversary pages

--- hatsune/_work/bin/bootstrap.sh
+++ hatsune/_work/bin/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp bower_components/bootstrap/dist/css/*.css ../css/
-cp bower_components/bootstrap/dist/js/*.js ../js/
-cp bower_components/jquery/dist/*.js ../js/
-cp bower_components/tether/dist/js/*.js ../js/
+cp bower_components/bootstrap/dist/css/*.css ../10th/css/
+cp bower_components/bootstrap/dist/js/*.js ../10th/js/
+cp bower_components/jquery/dist/*.js ../10th/js/
+cp bower_components/tether/dist/js/*.js ../10th/js/
