7122aae5032984056faa243aaacf456b6d822474
Yusuke Takahashi <youthkee@gmail.com>
2019-09-14 20:30:28 +0900

    Build 2019 special page

--- hatsune/_work/bin/bootstrap.sh
+++ hatsune/_work/bin/bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cp node_modules/bootstrap/dist/css/bootstrap.min.css ../826/css/
-cp node_modules/bootstrap/dist/js/bootstrap.bundle.min.js ../826/js/
+cp node_modules/bootstrap/dist/css/bootstrap.min.css ../birthday2019/css/
+cp node_modules/bootstrap/dist/js/bootstrap.bundle.min.js ../birthday2019/js/
